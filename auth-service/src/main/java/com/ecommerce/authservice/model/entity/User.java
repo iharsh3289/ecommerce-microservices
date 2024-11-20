59cf75a50fa94c14991463cd9bea8e1d76ff7b4c
Harsh Garg <hgarg5492@gmail.com>
2024-11-20 10:00:00 +0530

    Add Auth Service with JWT authentication and user management

---
+++ auth-service/src/main/java/com/ecommerce/authservice/model/entity/User.java
@@ -0,0 +1,70 @@
+package com.ecommerce.authservice.model.entity;
+
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(name = "unique_username", columnNames = "user_name"),
+        @UniqueConstraint(name = "unique_email", columnNames = "email"),
+        @UniqueConstraint(name = "unique_phone", columnNames = "phone_number")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    @NotBlank(message = "Full name must not be blank")
+    @Size(min = 3, max = 100, message = "Full name must be between 3 and 100 characters")
+    @Column(name = "full_name")
+    private String fullname;
+
+    @NotBlank(message = "Username must not be blank")
+    @Size(min = 3, max = 100, message = "Username must be between 3 and 100 characters")
+    @Column(name = "user_name")
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 50)
+    @Email(message = "Input must be in Email format")
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank(message = "Gender must not be blank")
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Pattern(regexp = "^\\+84[0-9]{9,10}$|^0[0-9]{9,10}$", message = "The phone number is not in the correct format")
+    @Size(min = 10, max = 11, message = "Phone number must be between 10 and 11 characters")
+    @Column(name = "phone_number", unique = true)
+    private String phone;
+
+    @Pattern(regexp = "^(http|https)://.*$", message = "Avatar URL must be a valid HTTP or HTTPS URL")
+    @Column(name = "image_url", columnDefinition = "TEXT")
+    private String avatar;
+
+    @Column(name = "keycloak_user_id", unique = true)
+    private String keycloakUserId;
+
+    @Builder.Default
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
